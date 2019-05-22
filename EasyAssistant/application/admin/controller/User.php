ce2b5e41b1c3a216a7860aed141536a20dfca6e8
zhufangzheng <13770543692@163.com>
2019-05-22 15:03:58 +0800

    提交文件

---
+++ EasyAssistant/application/admin/controller/User.php
@@ -0,0 +1,18 @@
+<?php
+namespace app\admin\controller;
+use think\Controller;
+class User extends Controller
+{
+	public function index(){
+	return $this->fetch();
+	}
+	public function list(){
+	return $this->fetch();
+	}
+	public function add(){
+	return $this->fetch();
+	}
+	public function edit(){
+	return $this->fetch();
+	}
+}
